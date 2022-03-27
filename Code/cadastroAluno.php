c67b687a7df4ea0d342c03313cad5bc28319ec92
FernandoFdeS <fernandofdes@hotmail.com>
2022-03-27 14:43:18 -0300

    Estiliza labels do cadastro Aluno

--- Code/cadastroAluno.php
+++ Code/cadastroAluno.php
@@ -4,31 +4,40 @@
 
 <div class="container">
     <div class="row mt-5">
-        <h1 class="mt-1">Cadastro de Aluno</h1>
+        <h1 class="mt-1 display-3">Cadastro de Aluno</h1>
         <form class="mt-5" action="#" method="#">
             <div class="form-group mt-3">
                 <label for="nome">Nome: </label>
-                <input type="text" class="form-control" id="nome" name="nome" required>
+                <input type="text" class="form-control" id="nome" name="nome" required placeholder="Nome do Aluno">
             </div>
 
             <div class="form-group mt-3">
-                <label for="cpf">CPF: </label>
-                <input type="text" class="form-control" id="cpf" name="cpf" required>
+            <label for="cpf" class="d-flex justify-content-between">
+                   <span>CPF:</span>
+                   <span class="text-muted display-3" style="font-size:15px;">XXX.XXX.XXX-XX</span>
+                </label>
+                <input type="text" class="form-control" id="cpf" name="cpf" required placeholder="XXX.XXX.XXX-XX">
             </div>
 
             <div class="form-group mt-3">
-                <label class="form-check-label" for="dataNasc">Data de Nascimento: </label>
+                <label for="dataNasc" class="d-flex justify-content-between">
+                   <span>Data de Nascimento:</span>
+                   <span class="text-muted display-3" style="font-size:15px;">DD/MM/AAAA</span>
+                </label>
                 <input type="date" class="form-control" id="dataNasc" name="dataNasc" required>                
             </div>
 
             <div class="form-group mt-3">
-                <label for="responsavel">Nome do Responsável: </label>
-                <input type="text" class="form-control" id="responsavel" name="responsavel" placeholder="Opcional">
+                <label for="responsavel" class="d-flex justify-content-between">
+                   <span>Responsável:</span>
+                   <span class="text-muted display-3" style="font-size:15px;">Opcional</span>
+                </label>
+                <input type="text" class="form-control" id="responsavel" name="responsavel" placeholder="...">
             </div>
 
             <div class="form-group mt-3">
-                <label for="responsavel">E-mail: </label>
-                <input type="email" class="form-control" id="responsavel" name="responsavel" placeholder="Opcional">
+                <label for="email">E-mail:</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="...">
             </div>
 
             <div class=" mt-5 mb-5">
