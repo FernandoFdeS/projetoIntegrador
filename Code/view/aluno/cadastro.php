2aa16f1ac4b09d10603ad60be0c10d763545bfe5
Lucas Bordin <lucastrentinibordin@hotmail.com>
2022-03-27 17:13:35 -0300

    adiciona campo de senha

--- Code/view/aluno/cadastro.php
+++ Code/view/aluno/cadastro.php
@@ -40,6 +40,11 @@
                 <input type="email" class="form-control" id="email" name="email" placeholder="...">
             </div>
 
+            <div class="form-group mt-3">
+                <label for="senha">Senha:</label>
+                <input type="text" class="form-control" id="senha" name="senha">
+            </div>
+
             <div class=" mt-5 mb-5">
                 <button type="submit" class="btn-principal">Cadastrar</button>
                 <span class="btn-secundario mx-1">
