f987d8366ed1c75a505babf7a72ff63aa7c2536a
FernandoFdeS <fernandofdes@hotmail.com>
2022-03-28 21:07:22 -0300

    Cria tabela das entidades principais no banco

--- Code/db/connection.php
+++ Code/db/connection.php
@@ -38,12 +38,76 @@ try {
                 cpf varchar(11) not null,
                 nome varchar(100) not null,
                 responsavel varchar(100),
-                username varchar(100) not null,
+                data_nasc date not null,
                 senha varchar(100) not null,
                 email varchar(100) not null                    
             );
+            CREATE TABLE professor(
+                id INTEGER PRIMARY KEY ,
+                cpf varchar(11) not null,
+                nome varchar(100) not null,
+                senha varchar(100) not null,
+                email varchar(100) not null,
+                contrato varchar(150)
+            );
+            CREATE TABLE curso(
+                id INTEGER PRIMARY KEY,
+                nome varchar(100) not null,
+                id_professor integer not null,
+                FOREIGN KEY (id) references professor(id_professor)
+            );
+            CREATE TABLE matricula(
+                id INTEGER PRIMARY KEY,
+                id_aluno integer not null,
+                id_curso integer not null,    
+                foreign key (id) references curso(id_curso),
+                foreign key (id) references aluno(id_aluno)
+            );
+            CREATE TABLE musica(
+                id INTEGER PRIMARY KEY,
+                nome varchar(100) not null,
+                artista varchar(100) not null,
+                anotacoes varchar(300) not null,
+                progessao smallint not null, -- 0 -> aprendendo, 1 -> completo, 2 -> em espera, 3 -> planejando aprender
+                cifra varchar(150),
+                link varchar(150),
+                arquivo_musica varchar(150),
+                instrumento smallint not null,
+                id_aluno integer not null,
+                foreign key (id) references aluno(id_aluno)
+            );
+            CREATE TABLE horario(
+                id INTEGER PRIMARY KEY,
+                dia smallint not null,
+                horario smallint not null   
+            );
+
+            CREATE TABLE professor_horario(
+                id INTEGER PRIMARY KEY,
+                id_horario integer not null,
+                id_professor integer not null,    
+                foreign key (id) references horario(id_horario),
+                foreign key (id) references professor(id_professor)
+            );
+
+            CREATE TABLE aula(
+                id INTEGER PRIMARY KEY,
+                id_curso integer not null,
+                id_horario integer not null, 
+                foreign key (id) references curso(id_curso),
+                foreign key (id) references horario(id_horario)
+            );
+
+            CREATE TABLE aluno_aula(
+                id INTEGER PRIMARY KEY,
+                id_aula integer not null,
+                id_aluno integer not null,
+                foreign key (id) references aluno(id_aluno),
+                foreign key (id) references aula(id_aula)
+            );
             INSERT INTO administrador(senha,email) values ("senha","adm");
-            INSERT INTO aluno(cpf,nome,username,senha,email) values ("00000000001","Aluno Teste","username_bobo","senha","aluno")'
+            INSERT INTO aluno(cpf,nome,data_nasc,senha,email) values ("00000000001","Aluno Teste","2002-01-01","senha","aluno");
+            INSERT INTO professor(cpf,nome,senha,email) values ("00000000002","Professor Teste","senha","professor")'
         );  
     }
 } catch (PDOException $e) {
