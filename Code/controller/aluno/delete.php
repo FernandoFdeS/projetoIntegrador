d66c87c5aa23dc16b7306eca5215c35616ee715f
Lucas Bordin <lucastrentinibordin@hotmail.com>
2022-04-05 19:41:40 -0300

    deleta aulas ao deletar aluno

--- Code/controller/aluno/delete.php
+++ Code/controller/aluno/delete.php
@@ -4,9 +4,24 @@
     if(isset($_GET['id'])){
         $id = $_GET['id'];
         try {
-            $sql=$db->prepare('DELETE FROM aluno WHERE id=:id');
-            $sql->bindParam(':id',$id);
-            $sql->execute();
+            $sql0=$db->prepare('SELECT id_aula FROM aluno_aula WHERE id_aluno=:id');
+            $sql0->bindParam(':id',$id);
+            $sql0->execute();
+            $aulas=$sql0->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach($aulas as $aula){
+                $sql=$db->prepare('DELETE FROM aula WHERE id=:id');
+                $sql->bindParam(':id',$aula['id_aula']);
+                $sql->execute();
+            }
+
+            $sql1=$db->prepare('DELETE FROM aluno_aula WHERE id_aluno=:id');
+            $sql1->bindParam(':id',$id);
+            $sql1->execute();
+
+            $sql2=$db->prepare('DELETE FROM aluno WHERE id=:id');
+            $sql2->bindParam(':id',$id);
+            $sql2->execute();
 
             header('Location:../../view/aluno/index.php');
         } catch (PDOException $e) {
